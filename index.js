fa91d25df0cfeaaa09d6423532d8dee4e83d2740
avilwock <151412286+avilwock@users.noreply.github.com>
2024-03-02 16:06:22 -0800

    made changes to shapes, circle, triangle, and square to appear in live server

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
 const inquirer = require('inquirer');
+const fs = require('fs');
+const Circle = require('./lib/Circle');
+const Triangle = require('./lib/Triangle');
+const Square = require('./lib/Square');
+const Shapes = require('./lib/shapes');
 
 async function askSvgQuestions() {
     const svgQuestions = [
@@ -7,7 +12,7 @@ async function askSvgQuestions() {
             message: 'Please enter up to 3 characters',
             name: 'characters',
             validate: function(input) {
-                const isValidLength = input.length > 3;
+                const isValidLength = input.length <= 3;
                 const isValidLetters = /^[a-zA-Z]+$/.test(input);
                 
                 if (!isValidLetters) {
@@ -17,6 +22,7 @@ async function askSvgQuestions() {
                 if (!isValidLength) {
                     return 'Please only enter up to only 3 characters';
                 }
+                return true;
            },
         },
         {
@@ -28,17 +34,60 @@ async function askSvgQuestions() {
             type: 'list',
             message: 'Please select a shape',
             name: 'shape',
-            choices: ['circle', 'square', 'triangle'],
+            choices: ['Circle', 'Square', 'Triangle'],
         },
         {
             type: 'input',
             message: 'Please enter a color for your shape',
             name: 'background-color',
         }
-
-    ]
-    const answers = await inquirer.prompt(svgQuestions);
-    console.log('Answers', answers);
+    ];
+    
+   return inquirer.prompt(svgQuestions);
 }
 
-askSvgQuestions();
+function generateSVG(answers) {
+    let svgContent = '';
+
+    if (answers && answers.shape) {
+    switch (answers.shape.toLowerCase()) {
+        case 'circle':
+            svgContent = new Circle(answers['background-color']).render();
+            break;
+        case 'square':
+            svgContent = new Square(answers['background-color']).render();
+            break;
+        case 'triangle':
+            svgContent = new Triangle(answers['background-color']).render();
+            break;
+        default:
+            console.error('Invalid shape selection');
+            break;
+    }
+
+    console.log('Color:', answers['background-color'])
+
+    console.log('Shape:', answers.shape);
+    console.log('SVG Content:', svgContent);
+
+    } else {
+        console.error('Shape not provided in answers');
+    }
+
+    
+    return svgContent || '';
+}
+
+async function init() {
+    const answers = await askSvgQuestions();
+    console.log('Answers:', answers);
+
+    const svgLogoContent = generateSVG(answers);
+    console.log('SVG Logo Content: ', svgLogoContent);
+
+    fs.writeFile('Logo.svg', svgLogoContent, (err) =>
+    err ? console.log(err): console.log('Success')
+    );
+
+}
+ init();
