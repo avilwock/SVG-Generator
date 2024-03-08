0e02015f396d9387551e2cb0fdc94ede02437f30
avilwock <151412286+avilwock@users.noreply.github.com>
2024-03-07 21:55:10 -0800

    added comments to the javascript files

--- index.js
+++ index.js
@@ -1,13 +1,16 @@
+//imports inquirer and fs packages
 const inquirer = require('inquirer');
 const fs = require('fs');
+//imports the circle, triangle, and square modules
 const Circle = require('./lib/Circle');
 const Triangle = require('./lib/Triangle');
 const Square = require('./lib/Square');
-const Shapes = require('./lib/shapes');
 
+//creates a function to ask SVG questions
 async function askSvgQuestions() {
     const svgQuestions = [
-        {
+        {   
+            //asks for letter input, ensures no more than 3 characters in length, and that everything is a letter character
             type: 'input',
             message: 'Please enter up to 3 characters',
             name: 'characters',
@@ -26,49 +29,54 @@ async function askSvgQuestions() {
            },
         },
         {
+            //asks for a font color to be entered
             type: 'input',
             message: 'Please enter a color for your font',
             name: 'font-color',
         },
         {
+            //asks for a selection between three options, a circle, a square, or a triangle
             type: 'list',
             message: 'Please select a shape',
             name: 'shape',
             choices: ['Circle', 'Square', 'Triangle'],
         },
         {
+            //asks for a color to be input for the shape fill
             type: 'input',
             message: 'Please enter a color for your shape',
             name: 'background-color',
         }
     ];
     
-   return inquirer.prompt(svgQuestions);
+    //calls the inquirer prompt for the questions
+    return inquirer.prompt(svgQuestions);
 }
 
+//this generates the SVG using the answers input into the file
 function generateSVG(answers) {
     let svgContent = '';
 
     if (answers && answers.shape) {
-        const upperCase = answers['characters'].toUpperCase();
-
+        
+        //ensures everything renders properly, and makes sure the entered text is converted to uppercase
         switch (answers.shape.toLowerCase()) {
             case 'circle':
-                svgContent = new Circle(answers['background-color'], upperCase, answers['font-color']).render();
+                svgContent = new Circle(answers['background-color'], answers['characters'].toUpperCase(), answers['font-color']).render();
                 break;
             case 'square':
-                svgContent = new Square(answers['background-color'], upperCase, answers['font-color']).render();
+                svgContent = new Square(answers['background-color'], answers['characters'].toUpperCase(), answers['font-color']).render();
                 break;
             case 'triangle':
-                svgContent = new Triangle(answers['background-color'], upperCase, answers['font-color']).render();
+                svgContent = new Triangle(answers['background-color'], answers['characters'].toUpperCase(), answers['font-color']).render();
                 break;
             default:
                 console.error('Invalid shape selection');
                 break;
     }
 
+    // console logs to ensure everything renders   
     console.log('Color:', answers['background-color'])
-
     console.log('Shape:', answers.shape);
     console.log('SVG Content:', svgContent);
 
@@ -80,6 +88,7 @@ function generateSVG(answers) {
     return svgContent || '';
 }
 
+//creates the run function, and writes the answers to the file.
 async function init() {
     const answers = await askSvgQuestions();
     console.log('Answers:', answers);
@@ -93,4 +102,5 @@ async function init() {
     );
 
 }
+//runs the file
  init();
